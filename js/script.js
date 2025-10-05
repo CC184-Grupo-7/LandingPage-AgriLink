af4b51f4514d8b90e61cf816714d71ec7f8773db
Diego201101 <colmop1548@gmail.com>
2025-10-05 18:13:47 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,18 @@ window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 60);
 }); 
 
-
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
-}; 
+};
+
+// Cerrar menú al hacer clic en un enlace (para móviles)
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('bx-x');
+        navbar.classList.remove('open');
+    });
+});
